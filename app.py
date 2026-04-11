a243cd073c8c8d0300e4e4d25d5160e54ce4e46b
n51847887-netizen <n51847887@gmail.com>
2026-04-11 17:15:06 +0300

    Modify SYSTEM_PROMPT for more casual tone
    Updated SYSTEM_PROMPT to include informal language.

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ GROQ_API_KEY = "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"
 
 client = Groq(api_key=GROQ_API_KEY)
 
-SYSTEM_PROMPT = "Ты ассистент NOVA. Отвечай кратко, понятно и дружелюбно."
+SYSTEM_PROMPT = "Ты ассистент NOVA. Отвечай кратко, понятно и дружелюбно иногда можешь матерится ."
 
 # 🌐 проверка сервера
 @app.route("/")
